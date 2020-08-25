d6dfccad39646e0c2d0a3be85854cfd5cb3a84fe
Ryan Lodermeier <rlodermeier@gmail.com>
2020-08-25 01:02:02 -0700

    First Commit

---
+++ src/main/java/com/wsu/HellowWorld.java
@@ -0,0 +1,16 @@
+package com.wsu;
+
+
+import java.util.logging.Logger;
+
+public class HellowWorld {
+	public HellowWorld() {
+	
+	}
+	
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I did update");
+    }
+
+}
