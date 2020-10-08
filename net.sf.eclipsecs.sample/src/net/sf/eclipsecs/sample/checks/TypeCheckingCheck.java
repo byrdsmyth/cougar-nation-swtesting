c76ac8aca4be5912979b261289b699ed94307022
Ryan Lodermeier <rlodermeier@gmail.com>
2020-10-07 17:50:43 -0700

    adding type check checkstyle

--- net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/TypeCheckingCheck.java
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/TypeCheckingCheck.java
@@ -1,5 +1,37 @@
-//package net.sf.eclipsecs.sample.checks;
-//
-//public class TypeCheckingCheck {
-//
-//}
+package net.sf.eclipsecs.sample.checks;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class TypeCheckingCheck extends AbstractCheck{
+
+	
+	  private int max = 1;
+	  
+	  @Override
+	  public int[] getAcceptableTokens() {
+	    return new int[] { TokenTypes.LITERAL_INSTANCEOF};
+	  }
+
+	  @Override
+	  public int[] getRequiredTokens() {
+	    return new int[0];
+	  }
+
+	  @Override
+	  public int[] getDefaultTokens() {
+	    return new int[] {TokenTypes.LITERAL_INSTANCEOF};
+	  }
+	  
+	  public void setMax(int limit) {
+		    max = limit;
+	  }
+	  
+	  @Override
+	  public void visitToken(DetailAST ast) {
+		  if (ast.getType() == TokenTypes.LITERAL_INSTANCEOF) {
+			  log(ast.getLineNo(), "Type Check found", max);
+		  }
+	  }
+}
