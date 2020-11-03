541f38e6462c39c5a26a8c979c5b47bb32f92e01
Ryan Lodermeier <rlodermeier@gmail.com>
2020-11-03 12:51:23 -0800

    Adding RefusedBequest

---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/test/RefusedBequestSuperTest.java
@@ -0,0 +1,16 @@
+package net.sf.eclipsecs.sample.test;
+
+public class RefusedBequestSuperTest {
+
+	public void methodOne() {
+		
+	}
+	
+	public void methodTwo() {
+		
+	}
+	
+	public void methodThree() {
+		
+	}
+}
---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/test/RefusedBequestTest.java
@@ -0,0 +1,14 @@
+package net.sf.eclipsecs.sample.test;
+
+public class RefusedBequestTest extends RefusedBequestSuperTest{
+
+	public RefusedBequestTest() {
+		this.methodOne();
+		this.methodTwo();
+	}
+	
+	@Override
+	public void methodThree() {
+		
+	}
+}
